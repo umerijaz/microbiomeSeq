0a32e793aac38e749ff0810033c22d71b4e53d00
bioguo <guokai8@gmail.com>
2019-08-02 13:21:42 -0500

    Update plot_anova_diversity.R

--- R/plot_anova_diversity.R
+++ R/plot_anova_diversity.R
@@ -26,45 +26,60 @@
 #' @export plot_anova_diversity
 #'
 
-plot_anova_diversity <- function(physeq, method, grouping_column,color=NULL,pValueCutoff=0.05,fontsize.x=10,fontsize.y=10,fill=NULL)
+plot_anova_diversity<-function (physeq, method, grouping_column, color = NULL, pValueCutoff = 0.05, 
+          fontsize.x = 10, fontsize.y = 10, fill = NULL) 
 {
-  #enforce orientation
-  if(taxa_are_rows(physeq)){
+  if (taxa_are_rows(physeq)) {
     physeq <- t(physeq)
   }
   abund_table <- otu_table(physeq)
   meta_table <- sample_data(physeq)
-
-  #get diversity measure using selected methods
-  div.df <- alpha_div(physeq,method)
-
-  #=add grouping information to alpha diversity measures
-  df<-data.frame(div.df,(meta_table[,grouping_column])[as.character(div.df$sample),])
-  df[,grouping_column]<-as.factor(df[,grouping_column])
-  #perform anova of diversity measure between groups
-  anova_res <- perform_anova(df,meta_table,grouping_column,pValueCutoff)
-  df_pw <- anova_res$df_pw #get pairwise p-values
-  if(is.null(color)){
-    color=grouping_column
-   }
-  #Draw the boxplots
-  p<-ggplot(aes_string(x=grouping_column,y="value",color=color),data=df)
-  if(!is.null(fill)){
-    p<-p+geom_boxplot(aes_string(fill=fill))+geom_jitter(position = position_jitter(height = 0, width=0))
-    }else{
-    p<-p+geom_boxplot()+geom_jitter(position = position_jitter(height = 0, width=0))
-    }
-  p<-p+theme_bw()
-  p<-p+theme(axis.text.x = element_text(angle = 90, hjust = 1,size=fontsize.x))
-  p<-p+theme(axis.text.y = element_text(size=fontsize.y))
-  p<-p+facet_wrap(~measure,scales="free_y",nrow=1)+ylab("Observed Values")+xlab("Samples")
-  p<-p+theme(strip.background = element_rect(fill = "white"))+xlab("Groups")
-
-  #This loop will generate the lines and signficances
-  if(!is.null(df_pw)){ #this only happens when we have significant pairwise anova results
-    for(i in 1:dim(df_pw)[1]){
-      p<-p+geom_path(inherit.aes=F,aes(x,y),data = data.frame(x = c(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"])),which(levels(df[,grouping_column])==as.character(df_pw[i,"to"]))), y = c(as.numeric(as.character(df_pw[i,"y"])),as.numeric(as.character(df_pw[i,"y"]))), measure=c(as.character(df_pw[i,"measure"]),as.character(df_pw[i,"measure"]))), color="black",lineend = "butt",arrow = arrow(angle = 90, ends = "both", length = unit(0.1, "inches")))
-      p<-p+geom_text(inherit.aes=F,aes(x=x,y=y,label=label),data=data.frame(x=(which(levels(df[,grouping_column])==as.character(df_pw[i,"from"]))+which(levels(df[,grouping_column])==as.character(df_pw[i,"to"])))/2,y=as.numeric(as.character(df_pw[i,"y"])),measure=as.character(df_pw[i,"measure"]),label=as.character(cut(as.numeric(as.character(df_pw[i,"p"])),breaks=c(-Inf, 0.001, 0.01, 0.05, Inf),label=c("***", "**", "*", "")))))
+  div.df <- alpha_div(physeq, method)
+  df <- data.frame(div.df, (meta_table[, grouping_column])[as.character(div.df$sample), 
+                                                           ])
+  df[, grouping_column] <- as.factor(df[, grouping_column])
+  anova_res <- perform_anova(df, meta_table, grouping_column, 
+                             pValueCutoff)
+  df_pw <- anova_res$df_pw
+  if (is.null(color)) {
+    color = grouping_column
+  }
+  p <- ggplot(aes_string(x = grouping_column, y = "value", 
+                         color = color), data = df)
+  if (!is.null(fill)) {
+    p <- p + geom_boxplot(aes_string(fill = fill)) + geom_point(aes_string(shape=fill))
+  }
+  else {
+    p <- p + geom_boxplot() + geom_point(aes_string(shape=color))
+  }
+  p <- p + theme_bw()
+  p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1, 
+                                            size = fontsize.x))
+  p <- p + theme(axis.text.y = element_text(size = fontsize.y))
+  p <- p + facet_wrap(~measure, scales = "free_y", nrow = 1) + 
+    ylab("Observed Values") + xlab("Samples")
+  p <- p + theme(strip.background = element_rect(fill = "white")) + 
+    xlab("Groups")
+  if (!is.null(df_pw)) {
+    for (i in 1:dim(df_pw)[1]) {
+      p <- p + geom_path(inherit.aes = F, aes(x, y), data = data.frame(x = c(which(levels(df[, 
+                                                                                             grouping_column]) == as.character(df_pw[i, "from"])), 
+                                                                             which(levels(df[, grouping_column]) == as.character(df_pw[i, 
+                                                                                                                                       "to"]))), y = c(as.numeric(as.character(df_pw[i, 
+                                                                                                                                                                                     "y"])), as.numeric(as.character(df_pw[i, "y"]))), 
+                                                                       measure = c(as.character(df_pw[i, "measure"]), 
+                                                                                   as.character(df_pw[i, "measure"]))), color = "black", 
+                         lineend = "butt", arrow = arrow(angle = 90, ends = "both", 
+                                                         length = unit(0.1, "inches")))
+      p <- p + geom_text(inherit.aes = F, aes(x = x, y = y, 
+                                              label = label), data = data.frame(x = (which(levels(df[, 
+                                                                                                     grouping_column]) == as.character(df_pw[i, "from"])) + 
+                                                                                       which(levels(df[, grouping_column]) == as.character(df_pw[i, 
+                                                                                                                                                 "to"])))/2, y = as.numeric(as.character(df_pw[i, 
+                                                                                                                                                                                               "y"])), measure = as.character(df_pw[i, "measure"]), 
+                                                                                label = as.character(cut(as.numeric(as.character(df_pw[i, 
+                                                                                                                                       "p"])), breaks = c(-Inf, 0.001, 0.01, 0.05, 
+                                                                                                                                                          Inf), label = c("***", "**", "*", "")))))
     }
   }
   return(p)
